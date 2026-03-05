3ff5a2e61998efe7ae688453560a7ac4f4fe882c
Rimjhim <91783657+dhakedrimjhim31@users.noreply.github.com>
2026-03-05 15:26:10 +0530

    Update app.py

--- Assignment/Demos/Day18/app.py
+++ Assignment/Demos/Day18/app.py
@@ -306,3 +306,131 @@ alert(JSON.stringify(data)); // popup output
 
 </body>
 </html>
+
+
+10//////////////
+from flask import Blueprint
+
+auth_bp = Blueprint('auth', _name_)
+
+from . import routes
+
+11////////////
+from flask import request, session, jsonify
+from . import auth_bp
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    username = request.json.get("username")
+
+    if not username:
+        return jsonify({"error": "Username required"}), 400
+
+    session['username'] = username
+
+    return jsonify({
+        "message": f"{username} logged in successfully"
+    })
+
+
+12/////////////////
+from flask import Blueprint
+
+products_bp = Blueprint('products', _name_)
+
+from . import routes
+
+13//////////////
+from flask import jsonify, request, session, make_response
+from . import products_bp
+
+products = [
+    {"id":1,"name":"Laptop","price":70000},
+    {"id":2,"name":"Mouse","price":500},
+    {"id":3,"name":"Keyboard","price":1200}
+]
+
+
+# API 2 - Get all products
+@products_bp.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+
+# API 3 - View product
+@products_bp.route('/product/<int:product_id>', methods=['GET'])
+def view_product(product_id):
+
+    if 'username' not in session:
+        return jsonify({"error":"Login required"}), 401
+
+    product = next((p for p in products if p["id"] == product_id), None)
+
+    if not product:
+        return jsonify({"error":"Product not found"}), 404
+
+    recent = request.cookies.get('recent_products')
+
+    if recent:
+        recent_list = recent.split(',')
+    else:
+        recent_list = []
+
+    pid = str(product_id)
+
+    if pid in recent_list:
+        recent_list.remove(pid)
+
+    recent_list.insert(0, pid)
+
+    if len(recent_list) > 5:
+        recent_list = recent_list[:5]
+
+    resp = make_response(jsonify(product))
+    resp.set_cookie('recent_products', ','.join(recent_list))
+
+    return resp
+
+
+# API 4 - Recently viewed products
+@products_bp.route('/recent-products', methods=['GET'])
+def recent_products():
+
+    recent = request.cookies.get('recent_products')
+
+    if not recent:
+        return jsonify([])
+
+    ids = recent.split(',')
+
+    result = []
+
+    for pid in ids:
+        for p in products:
+            if str(p["id"]) == pid:
+                result.append({
+                    "id": p["id"],
+                    "name": p["name"]
+                })
+
+    return jsonify(result)
+
+
+14////////////////
+from flask import Flask
+from auth import auth_bp
+from products import products_bp
+
+app = Flask(_name_)
+
+app.secret_key = "secret123"
+
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(products_bp)
+
+if _name_ == "_main_":
+    app.run(debug=True)
+
+
+
+
