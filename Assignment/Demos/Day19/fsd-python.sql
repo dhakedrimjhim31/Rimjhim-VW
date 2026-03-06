879be551203e6c56033df0fe6776cf2917b3f537
Rimjhim <91783657+dhakedrimjhim31@users.noreply.github.com>
2026-03-06 14:19:59 +0530

    Update fsd-python.sql

--- Assignment/Demos/Day19/fsd-python.sql
+++ Assignment/Demos/Day19/fsd-python.sql
@@ -23,7 +23,9 @@ alter table employees
 change emp_id emp_id int auto_increment;
  
 desc employees;
- 
+
+
+
 insert into employees(emp_name,salary)
 values('amit',34000.78);
  
@@ -64,3 +66,101 @@ select * from employees;
  
 delete from employees
 where emp_id=4;
+
+###
+alter table employees
+drop column dept;
+
+alter table employees
+change salary emp_salary int;
+
+
+select sum(duration),subject_name
+from tutorials
+group by subject_name
+having sum(duration)>20;
+
+#########
+•SELECT column-names
+  FROM table-name
+  WHERE condition
+  GROUP BY column-names
+  HAVING condition
+ 
+•SELECT column-names
+  FROM table-name
+  WHERE condition
+  GROUP BY column-names
+  HAVING condition
+  ORDER BY column-names
+
+########
+Select column-list
+  fromtable_name
+  where condition
+  [group by column]
+  [order by column];
+
+
+
+####
+select sum(duration),subject_name
+from tutorials
+where subject_name<>'python'
+group by subject_name
+having sum(duration)>20;
+
+#######
+create table customers(id int primary key auto_increment,cust_name varchar(30),cust_city varchar(30));
+ 
+create table orders(id int primary key auto_increment,
+ord_name varchar(30),
+ord_price int,
+ord_qty int,
+cust_id int,
+foreign key(cust_id)references customers(id));
+ 
+insert into customers(cust_name,cust_city)
+values('ravi','mumbai'),
+('amit','lucknow'),
+('priya','mumbai'),
+('alok','agra')
+ 
+select * from customers;
+ 
+insert into orders
+(ord_name,ord_price,ord_qty,cust_id)
+values('laptop',60000,1,2),('mobile cover',1000,2,3);
+
+#########
+select cust_name,ord_name,ord_qty
+from customers
+inner join orders
+on customers.id=orders.cust_id;
+
+select cust_name,ord_name,ord_qty
+from customers,orders
+where customers.id=orders.cust_id;
+ 
+ 
+select cust_name,ord_name,ord_qty
+from customers
+left join orders
+on customers.id=orders.cust_id;
+
+select cust_name,ord_name,ord_qty
+from customers
+right join orders
+on customers.id=orders.cust_id;
+
+#####
+select emp_name from employees where
+emp_salary=(select max(emp_salary) from employees);
+ 
+
+#####
+select dept_name
+from department,employees
+where department.id=employees.emp_id
+group by dept_name
+having count(*)<2;
