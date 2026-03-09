20e5c91e1265f468970f9b8cf02828bbd3f65213
Rimjhim <91783657+dhakedrimjhim31@users.noreply.github.com>
2026-03-10 00:15:15 +1300

    Update app.py

--- Assignment/Day20/app.py
+++ Assignment/Day20/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
  
 app=Flask(__name__)
  
-app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://flaskuser:root@localhost/fsd"
+app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:root@localhost/fsd"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
  
 db=SQLAlchemy(app)
@@ -62,4 +62,5 @@ def delete_student(id):
  
 if __name__=="__main__":
     app.run(debug=True)
+
  
